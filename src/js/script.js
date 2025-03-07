8a6b1a037782ddaf988c71e4b1435b90fdbd4a41
isu-Hugo <ehugo209@gmail.com>
2025-03-07 14:29:25 -0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let soundMata = new Audio("/src/sounds/mata.mp3");
+let soundMama = new Audio("/src/sounds/mama.mp3");
+
+function mama(){
+    soundMama.play();
+}
+
+function mata(){
+    soundMata.play();
+}
